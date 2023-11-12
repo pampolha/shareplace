ba3325be419975335f4f2a9bb92abb1df3623bd0
Gabriel Pampolha <pampolhag@gmail.com>
2023-11-12 17:02:14 -0300

    feat: made db properties required

--- models/users.js
+++ models/users.js
@@ -1,14 +1,23 @@
 const dynamoose = require("dynamoose");
 const usersSchema = new dynamoose.Schema(
   {
-    id: { type: String, hashKey: true },
-    name: String,
-    email: String,
-    password: String,
+    id: { type: String, hashKey: true, required: true },
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
   }
 );
-const Users = dynamoose.model('Users', usersSchema);
+const Users = dynamoose.model("Users", usersSchema);
 module.exports = Users;
